a1a3a00a074b8f81265f537cafaa182fc32176be
Jack Clayton <jackos@me.com>
2021-10-20 20:56:23 +0800

    Deref and drop

--- example_projects/playground/src/main.rs
+++ example_projects/playground/src/main.rs
@@ -1,12 +1,20 @@
 #[derive(Debug)]
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+struct CustomSmartPointer {
+    data: String,
 }
 
-use crate::List::{Cons, Nil};
-fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
 
-    println!("{:?}", list)
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created: {:?} and {:?}", c, d);
 }
